676aa0031118b6fcab383c2f8a4c801dae7f1cde
Lucas <lucaspedrazoli@gmail.com>
2020-10-05 16:46:07 -0300

    examples of using stride

--- heap-starter.playground/Contents.swift
+++ heap-starter.playground/Contents.swift
@@ -2,4 +2,20 @@
 // For full license & permission details, see LICENSE.markdown.
 
 
-print("teste")
+example(of: "stride to") {
+  for i in stride(from: 0, to: 10, by: 2) {
+      print(i)
+  }
+}
+
+example(of: "stride to with decimal") {
+  for i in stride(from: 0, to: 0.5, by: 0.1) {
+      print(i)
+  }
+}
+
+example(of: "stride through") {
+  for i in stride(from: 0, through: 10, by: 2) {
+      print(i)
+  }
+}
